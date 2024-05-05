d830329c4986d11fe1377beefbb6d5c10b1a8c0b
Shawn Grycki <sglightning@gmail.com>
2024-05-04 23:20:30 -0500

    updates calculator program

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -4,25 +4,51 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f"==> {message}")
 
-print("What's the first number?")
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
 number1 = input()
 
-print("What's the second number?")
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+
+prompt("What's the second number?")
 number2 = input()
 
-print('What operation would you like to perform?\n'
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt('What operation would you like to perform?\n'
       '1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
-if operation == '1':   # '1' represents addition
-    output = int(number1) + int(number2)
-elif operation == '2': # '2' represents subtraction
-    output = int(number1) - int(number2)
-elif operation == '3': # '3' represents multiplication
-    output = int(number1) * int(number2)
-elif operation == '4': # '4' represents division
-    output = int(number1) / int(number2)
+while operation not in ['1', '2', '3', '4']:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
 
-print(f"The result is: {output}")
+
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output= int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
+
+prompt(f'The result is: {output}')
