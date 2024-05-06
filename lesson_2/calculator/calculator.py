3fd06f0fa341ae9e1e248b01860bc43620e0d0ef
Shawn Grycki <sglightning@gmail.com>
2024-05-06 17:08:11 -0500

    Base program refactor

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -1,54 +1,73 @@
-# Ask the user for the first number.
-# Ask the user for the second number.
-# Ask the user for an operation to perform.
-# Perform the operation on the two numbers.
-# Print the result to the terminal.
+# Welcome the user and give config operations
+# Ask the user for the first number
+# Check if valid numeric
+# Ask the user for the second number
+# Check if valid numeric
+# Ask the user for an operation to perform
+# Check if valid operator
+# Perform the operation on the two numbers
+# Print the result to the terminal as float
+# Ask if user wants another calculation. Ask for first number if yes
+# Else, exit program with message
+
+import pdb
 
 def prompt(message):
-    print(f"==> {message}")
+    print(f'==> {message}')
+
+def input_format(message):
+    return input(f'==> {message}')
+
+def get_number(order):
+    while True:
+        number = input_format(f"What's the {order} number? ")
+
+        if invalid_number(number):
+            prompt("Hmm... that doesn't look like a valid number.")
+        else:
+            return float(number)
+
+def get_operator():
+    while True:
+        operation = input_format('What operation would you like to perform?\n'
+                                 '1) Add 2) Subtract 3) Multiply 4) Divide ')
+        
+        if invalid_operator(operation):
+            prompt('You must choose 1, 2, 3, or 4')
+        elif zero_division(operation):
+            prompt('Cannot divide by 0') 
+        else:
+            return operation
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
 
+def invalid_operator(operation):
+    return operation not in ['1', '2', '3', '4']
+
+def zero_division(operation):
+    return operation == '4' and number2 == 0
+
+def calculation():
+    match operation:
+        case '1':
+            prompt(f'The result is: {number1 + number2}')
+        case '2':
+            prompt(f'The result is: {number1 - number2}')
+        case '3':
+            prompt(f'The result is: {number1 * number2}')
+        case '4':
+            prompt(f'The result is: {number1 / number2}')
+
+
+# main body
 prompt('Welcome to Calculator!')
-
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
-    number1 = input()
-
-
-prompt("What's the second number?")
-number2 = input()
-
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
-    number2 = input()
-
-prompt('What operation would you like to perform?\n'
-      '1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
-
-while operation not in ['1', '2', '3', '4']:
-    prompt('You must choose 1, 2, 3, or 4')
-    operation = input()
-
-
-match operation:
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output= int(number1) * int(number2)
-    case '4':
-        output = int(number1) / int(number2)
-
-prompt(f'The result is: {output}')
+number1 = get_number('first')
+number2 = get_number('second')
+operation = get_operator()
+calculation()
