6bb2cce1ed75657e6a4015ebd5cccecb40bb8976
Shawn Grycki <sglightning@gmail.com>
2024-05-12 14:49:34 -0500

    Fixes 0 interest bug

--- lesson_2/loan_calculator/loan_calculator.py
+++ lesson_2/loan_calculator/loan_calculator.py
@@ -11,18 +11,15 @@ MONTHS_IN_YEAR = 12
 MIN_INPUT = 0
 
 # <============ MESSAGE FUNCTIONS ============>
-# Resets screen with support for Windows, Mac, and Linux
 def reset_screen():
     if os.name == 'nt':
         os.system("cls") # Windows
     else:
         os.system("clear") # Mac/Linux
 
-# Prints out message with proper formatting
 def print_prompt(message):
     print(f'==> {message}')
 
-# Returns input from a passed message with proper formatting
 def input_prompt(message):
     if isinstance(message, list):
         message = format_message(message)
@@ -38,7 +35,6 @@ def format_values(amount, rate, payment):
     formatted = f'{amount:.2f},{rate:.2f},{payment:.2f}'
     return formatted.split(',')
 
-# Outputs final calculation with relevant input data
 def print_result(amount, rate, years, months, payment):
     amount, rate, payment = format_values(amount, rate, payment)
 
@@ -48,7 +44,6 @@ def print_result(amount, rate, years, months, payment):
     print_prompt(f'Your monthly payment is: ${payment}')
 
 # <============ DATA RETRIEVAL FUNCTIONS ============>
-# Gets loan amount as float rounded 2 places
 def get_loan_amount():
     while True:
         amount = input_prompt(MESSAGES['loan_amount']).strip('$')
@@ -59,7 +54,6 @@ def get_loan_amount():
 
         print_prompt(MESSAGES['amount_error'])
 
-# Gets APR as float rounded 2 places
 def get_yearly_rate():
     while True:
         rate = input_prompt(MESSAGES['rate_amount']).strip('%')
@@ -69,7 +63,6 @@ def get_yearly_rate():
 
         print_prompt(MESSAGES['rate_error'])
 
-# Gets the year(s) of the loan duration
 def get_years():
     while True:
         years = input_prompt(MESSAGES['year_amount'])
@@ -79,7 +72,6 @@ def get_years():
 
         print_prompt(MESSAGES['year_error'])
 
-# Gets the month(s) of the loan duration
 def get_months(years):
     while True:
         months = input_prompt(MESSAGES['month_amount'])
@@ -89,13 +81,11 @@ def get_months(years):
 
         print_prompt(MESSAGES['month_error'])
 
-# Resets main program if user says y/yes
 def calculate_again():
     answer = input_prompt(MESSAGES['do_again'])
     return answer in ('y', 'yes')
 
 # <============ VALIDATION FUNCTIONS ============>
-# Checks if user entered valid loan amount (float greater than 0)
 def valid_loan_amount(amount):
     try:
         amount = float(amount)
@@ -107,7 +97,6 @@ def valid_loan_amount(amount):
 
     return True
 
-# Checks if user entered valid APR (float of at least 0)
 def valid_rate(rate):
     try:
         rate = float(rate)
@@ -119,7 +108,6 @@ def valid_rate(rate):
 
     return True
 
-# Checks if user entered valid number of years (int 0-99)
 def valid_year_amount(years):
     try:
         years = int(years)
@@ -131,7 +119,6 @@ def valid_year_amount(years):
 
     return True
 
-# Checks if user entered valid number of months (int 0-11)
 def valid_month_amount(months):
     try:
         months = int(months)
@@ -148,31 +135,30 @@ def valid_duration(years, months):
     return years > MIN_INPUT or int(months) > MIN_INPUT
 
 # <============ CALCULATION FUNCTIONS ============>
-# Calculates total months from years and months
 def calculate_total_months(years, months):
     return (years * MONTHS_IN_YEAR) + months
 
-# Calculates monthly rate from yearly rate
 def calculate_monthly_rate(yearly_rate):
     return (yearly_rate / 100) / MONTHS_IN_YEAR
 
-# Calculates monthly payments
 def calculate_payments(amount, rate, duration):
-    monthly_payment = amount * (rate / (1 - (1 + rate) ** (-duration)))
+    if rate == 0: # Allows for 0 interest calculation without error
+        monthly_payment = amount / duration
+    else:
+        monthly_payment = amount * (rate / (1 - (1 + rate) ** (-duration)))
     return monthly_payment
 
 # <============ MAIN PROGRAM LOGIC ============>
-# Main orchestration function
 def start():
     print_prompt(MESSAGES['welcome'])
 
     while True:
-        amount = get_loan_amount() # (as float)
+        amount = get_loan_amount()
         reset_screen()
-        yearly_rate = get_yearly_rate() # (as float)
+        yearly_rate = get_yearly_rate()
         reset_screen()
-        years = get_years() # (as integer)
-        months = get_months(years) # (as integer)
+        years = get_years()
+        months = get_months(years)
         reset_screen()
 
         duration = calculate_total_months(years, months)
@@ -188,5 +174,4 @@ def start():
 
     print_prompt(MESSAGES['goodbye'])
 
-# Begin program
 start()
